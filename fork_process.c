edeb8505f657b9877be7ca9175583cbd9ab03f13
TanjereeN <96239912+TanjereeN98@users.noreply.github.com>
2023-05-23 02:01:23 +0300

    Update fork_process.c

--- fork_process.c
+++ fork_process.c
@@ -10,8 +10,7 @@ void fork_process(char **args, char **argv, char **env)
 {
 	pid_t my_pid;
 	paths current_paths = handle_path(env, args);
-	size_t i, flag = access(current_paths.my_paths[i - 1], X_OK) != 0;
-	int len_2;
+	size_t i, flag;
 
 	for (i = 0 ; i < current_paths.size ; i++)
 	{
@@ -34,22 +33,8 @@ void fork_process(char **args, char **argv, char **env)
 			}
 		}
 	}
-	len_2 = _strlen(argv[0]);
+	flag = access(current_paths.my_paths[i - 1], X_OK) != 0;
 	if (i == current_paths.size && flag)
 		perror(argv[0]);
 	qol_free(current_paths.my_paths, current_paths.size);
 }
-
-/**
-*history_s = _itoa(history);len = _strlen(argv[0]) + 
-*_strlen(history_s) + _strlen(args[0]) + 24;
-*last = malloc(sizeof(char) * (len + 1));
-*p_name = _strdup(argv[0]);
-*args_dup = _strdup(args[0]);
-*last = _strcat(last, history_s);
-*last = _strcat(last, ": ");
-*last = _strcat(last, args[0]);
-*last = _strcat(last, ": not found\n");
-*p_name = _strcat(p_name, last);
-*len_2 = _strlen(p_name) + 1;
-*/
