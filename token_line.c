3193d2369181743e6ce65e8c9ab16525013594a7
TanjereeN <96239912+TanjereeN98@users.noreply.github.com>
2023-05-23 01:34:25 +0300

    Update token_line.c

--- token_line.c
+++ token_line.c
@@ -3,76 +3,43 @@
 /**
  * token_line - function to tokenize the input.
  * @line: the buffer line read to be tokenized.
+ * @delim: the delimeter used
  * Return: pointer to array of strings.
  */
 
-char **token_line(char *line)
+char **token_line(char *line, char *delim)
 {
-	char *token = NULL, *buff_dup = NULL, *delim = " ";
+	char *token = NULL, *line_dup = NULL;
 	char **tokens = NULL;
-	int len = 0, buffsize = 64, index = 0;
+	int idx = 0, len = 0, number = 0;
+	size_t size = (tokens_count(line, delim) + 1);
 
-	buff_dup = _strdup(line);
-	tokens = malloc(buffsize * sizeof(char *));
+	len = _strlen(line);
+	line_dup = _calloc(len + 1, sizeof(char));
+	line_dup = _strdup(line);
+	tokens = allocate_string_array(size);
 	if (!tokens)
 	{
 		write(STDERR_FILENO, "Allocation Error\n", 17);
 		exit(EXIT_FAILURE);
 	}
-	token = strtok(buff_dup, delim);
+	if (line_dup != NULL)
+		token = strtok(line_dup, delim);
 	while (token != NULL)
 	{
-		tokens[len] = malloc(sizeof(char) * (_strlen(token) + 1));
-		if (!tokens[len])
+		number = _strlen(token);
+		tokens[idx] = malloc(sizeof(char) * (number + 1));
+		if (!tokens[idx])
 		{
-			for (index ; index <= buffsize ; index++)
-				free(tokens[index]);
-			free(tokens);
+			qol_free(tokens, idx);
 			write(STDERR_FILENO, "Allocation Error\n", 17);
 			exit(EXIT_FAILURE);
 		}
-		_strcpy(tokens[len], token);
-		len++;
+		_strcpy(tokens[idx], token);
+		idx++;
 		token = strtok(NULL, delim);
 	}
-	tokens[len] = NULL;
-	free(buff_dup);
+	tokens[idx] = NULL;
+	free(line_dup);
 	return (tokens);
 }
-
-
-/**char **token_line(char *line)
-{
-	char *token, *buff_dup = NULL, *delim = " ";
-	char **tokens = NULL;
-	int len = 0, index = 0, buffsize = 0, letters = 0;
-
-	buff_dup = _strdup(line);
-	buffsize = tokens_count(buff_dup, delim);
-	tokens = malloc((buffsize + 1) * sizeof(char *));
-	if (!tokens)
-	{
-		write(STDERR_FILENO, "Allocation Error\n", 17);
-		exit(EXIT_FAILURE);
-	}
-	token = strtok(buff_dup, " ");
-	while (token != NULL)
-	{
-		letters = _strlen(token);
-		tokens[len] = malloc(sizeof(char) * (letters + 1));
-		if (!tokens[len])
-		{
-			for (index ; index <= buffsize ; index++)
-				free(tokens[index]);
-			free(tokens);
-			write(STDERR_FILENO, "Allocation Error\n", 17);
-			exit(EXIT_FAILURE);
-		}
-		_strcpy(tokens[len], token);
-		len++;
-		token = strtok(NULL, " ");
-	}
-	tokens[len] = NULL;
-	free(buff_dup);
-	return (tokens);
-}*/
