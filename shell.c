62e1d631193d7a486a561cf6d768547a0bf198b8
TanjereeN <96239912+TanjereeN98@users.noreply.github.com>
2023-05-24 04:17:50 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -16,10 +16,15 @@ int main(int argc, char *argv[], char **env)
 	void (*funptr)(int, char **);
 	int exit_status = 0;
 
-	history = 1;
+	if (argc > 1)
+	{
+		perror("Here non-inter");
+	}
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
+		if (!isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "", 0);
+		else if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
 		buff = read_line();
 		if (buff == NULL)
@@ -28,15 +33,10 @@ int main(int argc, char *argv[], char **env)
 		exit_status = tokens[1] ? _atoi(tokens[1]) : 0;
 		funptr = find_fun(tokens[0]);
 		if (funptr != NULL)
-		{
-			history++;
 			funptr(exit_status, env);
-		}
-		else
+		else if (tokens != NULL)
 			fork_process(tokens, argv, env);
 		free(buff);
-		if (!isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
 	}
 	exit(EXIT_FAILURE);
 }
